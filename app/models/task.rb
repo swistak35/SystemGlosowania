b58f705fe7356f847337e15f9a0ae9f32bf7c1bd
Piotr Pytlik <piotrcpytlik@gmail.com>
2014-12-11 08:45:36 +0100

    removed files

--- app/models/task.rb
+++
@@ -1,5 +0,0 @@
-class Task < ActiveRecord::Base
-  def mark_as_done!
-    self.update_attributes(done: true)
-  end
-end
--- db/migrate/20141127075039_add_published_to_tasks.rb
+++
@@ -1,5 +0,0 @@
-class AddPublishedToTasks < ActiveRecord::Migration
-  def change
-    add_column :tasks, :done, :boolean, default: false, null: false
-  end
-end
